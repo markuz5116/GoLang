6119c5d9dbc4c5d6eadd489f5212615c658f5784
Marcus Ong <markuz5116@gmail.com>
2022-08-13 15:22:37 +0800

    Creating basic main file

---
+++ main.go
@@ -0,0 +1,9 @@
+// If package is called main, it will create an executable
+package main
+
+import "fmt"
+
+// There can only be one main functions, so that Golang knows which main to call
+func main() {
+	fmt.Println("Hello World")
+}
