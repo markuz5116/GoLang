ffcafc1da15cb24cdc49e558f7d8516cca838e7b
Marcus Ong <markuz5116@gmail.com>
2022-08-13 15:29:56 +0800

    Using string vars

--- main.go
+++ main.go
@@ -5,5 +5,19 @@ import "fmt"
 
 // There can only be one main functions, so that Golang knows which main to call
 func main() {
-	fmt.Println("Hello World")
+	// Declare variables: var {name} {type}
+	var explicitName string = "explicit string"
+	fmt.Println(explicitName)
+
+	var implicitName = "implicit string"
+	fmt.Println(implicitName)
+
+	explicitName = "explicit string 2"
+
+	// Do := only for initializing, after which use =
+	// This cannot be used outside a function
+	nameWithVar := "No var string"
+	fmt.Println(nameWithVar)
+
+	nameWithVar = "No var updated string"
 }
