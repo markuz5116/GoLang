c3bdcd03ef7227ace788d264866ee5d7a01fec7b
Marcus Ong <markuz5116@gmail.com>
2022-08-13 15:38:33 +0800

    Using int and ints with bits

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "fmt"
 
 // There can only be one main functions, so that Golang knows which main to call
 func main() {
+	// string: ------------------------------------------------
 	// Declare variables: var {name} {type}
 	var explicitName string = "explicit string"
 	fmt.Println(explicitName)
@@ -20,4 +21,18 @@ func main() {
 	fmt.Println(nameWithVar)
 
 	nameWithVar = "No var updated string"
+
+	// int: ----------------------------------------------------
+	var explicitInt int = 1
+	var implicitInt = 2
+	noVarInt := 3
+	fmt.Println(explicitInt + implicitInt + noVarInt)
+
+	// typesOfInt: (no. of bits used) --------------------------
+	var num8Bit int8 = 127
+	num8Bit += 1
+	fmt.Println(num8Bit) // Overflowed
+
+	var num8Unsigned uint8 = 255
+	fmt.Println(num8Unsigned)
 }
